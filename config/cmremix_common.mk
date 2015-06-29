7c2b3416b940612c49f23bf0c0d4beb1f5beb32d
Readone !! ErcOne <android-armv7a-belalang-tempur@users.noreply.github.com>
2015-06-29 08:07:49 +0700

    BLISS

--- config/cmremix_common.mk
+++ config/cmremix_common.mk
@@ -1,43 +1,43 @@
 # Zion959 Debugs Files
 PRODUCT_COPY_FILES += \
-    vendor/cmremix/prebuilt/common/etc/00_DEBUG:system/etc/init.d/00_DEBUG
+    vendor/blissremix/prebuilt/common/etc/00_DEBUG:system/etc/init.d/00_DEBUG
 
 # AdBlocker Files
 PRODUCT_COPY_FILES += \
-    vendor/cmremix/prebuilt/common/etc/hosts.alt:system/etc/hosts.alt \
-    vendor/cmremix/prebuilt/common/etc/hosts.og:system/etc/hosts.og
+    vendor/blissremix/prebuilt/common/etc/hosts.alt:system/etc/hosts.alt \
+    vendor/blissremix/prebuilt/common/etc/hosts.og:system/etc/hosts.og
 
 # CM-Remix Overlays
-PRODUCT_PACKAGE_OVERLAYS += vendor/cmremix/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/blissremix/overlay/common
 
 # Bootanimation
-PRODUCT_COPY_FILES += vendor/cmremix/prebuilt/common/bootanimation/$(CMREMIX_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
+PRODUCT_COPY_FILES += vendor/blissremix/prebuilt/common/bootanimation/$(BLISSREMIX_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
 
 # SuperSU
 PRODUCT_COPY_FILES += \
-    vendor/cmremix/prebuilt/common/etc/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
-    vendor/cmremix/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+    vendor/blissremix/prebuilt/common/etc/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+    vendor/blissremix/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
 
 # Init script file with CM-Remix extras
 PRODUCT_COPY_FILES += \
-vendor/cmremix/prebuilt/common/etc/init.local.rc:root/init.cmremix.rc
+vendor/blissremix/prebuilt/common/etc/init.local.rc:root/init.blissremix.rc
 
 # easy way to extend to add more packages
--include vendor/cmremix/extra/product.mk
+-include vendor/blissremix/extra/product.mk
 
 # Debugs Script
--include vendor/cmremix/products/debug.mk
+-include vendor/blissremix/products/debug.mk
 
 # CM-Remix version
-CMREMIXVERSION := $(shell echo $(CMREMIX_VERSION) | sed -e 's/^[ \t]*//;s/[ \t]*$$//;s/ /./g')
-BOARD := $(subst cmremix_,,$(TARGET_PRODUCT))
-CMREMIX_BUILD_VERSION := CMRemix-$(BOARD)-$(CMREMIXVERSION)-$(shell date +%Y%m%d-%H%M%S)
+BLISSREMIXVERSION := $(shell echo $(BLISSREMIX_VERSION) | sed -e 's/^[ \t]*//;s/[ \t]*$$//;s/ /./g')
+BOARD := $(subst blissremix_,,$(TARGET_PRODUCT))
+BLISSREMIX_BUILD_VERSION := CMRemix-$(BOARD)-$(BLISSREMIXVERSION)-$(shell date +%Y%m%d-%H%M%S)
 
 # Set the board version
 CM_BUILD := $(BOARD)
 
 # Lower RAM devices
-ifeq ($(CMREMIX_LOW_RAM_DEVICE),true)
+ifeq ($(BLISSREMIX_LOW_RAM_DEVICE),true)
 MALLOC_IMPL := dlmalloc
 TARGET_BOOTANIMATION_TEXTURE_CACHE := false
 
@@ -52,15 +52,15 @@ endif
 
 # ROMStats Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.cmremixstats.name=cmRemix Rom \
-    ro.cmremixstats.version=$(CMREMIXVERSION) \
-    ro.cmremixstats.tframe=1
+    ro.blissremixstats.name=cmRemix Rom \
+    ro.blissremixstats.version=$(BLISSREMIXVERSION) \
+    ro.blissremixstats.tframe=1
 
 # statistics identity
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.cmremix.version=$(CMREMIXVERSION) \
-    ro.modversion=$(CMREMIX_BUILD_VERSION) \
-    cmremix.ota.version=$(CMREMIX_BUILD_VERSION)
+    ro.blissremix.version=$(BLISSREMIXVERSION) \
+    ro.modversion=$(BLISSREMIX_BUILD_VERSION) \
+    blissremix.ota.version=$(BLISSREMIX_BUILD_VERSION)
 
 # Disable ADB authentication and set root access to Apps and ADB
 ifeq ($(DISABLE_ADB_AUTH),true)
@@ -69,7 +69,7 @@ ifeq ($(DISABLE_ADB_AUTH),true)
         persist.sys.root_access=3
 endif
 
-EXTENDED_POST_PROCESS_PROPS := vendor/cmremix/tools/cmremix_process_props.py
+EXTENDED_POST_PROCESS_PROPS := vendor/blissremix/tools/blissremix_process_props.py
 
 # Inherite sabermod vendor
 SM_VENDOR := vendor/sm
